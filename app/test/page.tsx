ad5e7978b120907e18b46cef1713f8429b0fa0f6
SunJet <artemrabinin450@gmail.com>
2026-01-24 12:44:01 +0200

    Deleted unnecessary folders.

--- app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Page() {
-  return (
-    <div className="flex items-center">
-      <div className="w-1/4 bg-amber-500">df</div>
-      <div className="w-1/4 bg-amber-500 text-5xl">df</div>
-      <div className="w-1/4 bg-amber-500">df</div>
-    </div>
-  );
-}
